db46b8aa3c71e88730471d80cf45d57a8f61d7bd
Gabriel Reguse da Silva <gabrielreguse1@gmail.com>
2026-03-05 07:16:56 -0300

    Atualizar o cadastro.js
    Gabriel E

--- js/cadastro.js
+++ js/cadastro.js
@@ -13,6 +13,7 @@ const emailsPermitidos = new Set([
     "fusinatorycardo5@gmail.com",
     "ardasilvamiguel@gmail.com",
     "leohenriqueandrade.2010@gmail.com",
+    "rechenberg.gabriel@gmail.com",
     "",
 
     // SUPER ADM MASTER
